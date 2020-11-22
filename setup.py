b6ec7662f05aa5de75c44607a6352c3c47eeebe1
ahadmushir <a.ahad.mushir@gmail.com>
2020-11-22 21:46:43 +0100

    updating setup.py email

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='mypackage',
 version='0.0.1',
 description='Testing installation of Package',
-url='#',
+url='https://github.com/ahadmushir/my_first_package',
 author='ahadmushir',
 author_email='a.ahad.mushir@gmail.com',
 license='MIT',
