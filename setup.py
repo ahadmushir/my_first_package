bb2e8fb9f4ba5eea8d1f057b990e00f4c6dd7ec4
ahadmushir <a.ahad.mushir@gmail.com>
2020-11-22 21:03:46 +0100

    adding files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='mypackage',
+version='0.0.1',
+description='Testing installation of Package',
+url='#',
+author='ahadmushir',
+author_email='a.ahad.mushir@gmail.com',
+license='MIT',
+packages=['mypackage'],
+zip_safe=False)
---
+++ test.py
@@ -0,0 +1,4 @@
+import mypackage
+
+print (mypackage.average(10,20))
+mypackage.greet()
